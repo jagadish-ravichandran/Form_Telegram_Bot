b46e754d3da44d26fd415308790de63317d3ed23
Jagadish R <goldjagadish@gmail.com>
2021-06-17 11:50:21 +0530

    added view forms command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
 
-from functions import db_connect, start_command, creating_form, cancel_command, no_of_questions, questions_started, answering, title_of_form
+from functions import db_connect, start_command, creating_form, cancel_command, no_of_questions, questions_started, answering, title_of_form, view_forms
 
 from telegram import Bot
 
@@ -64,17 +64,25 @@ def main():
     updater = Updater(api_token, use_context=True)
     
     d = updater.dispatcher
-    #d.add_handler(CommandHandler("start", start_command))
+
     d.add_handler(ConversationHandler(
-        entry_points=[(CommandHandler("start", start_command))],
+        entry_points=[CommandHandler("start", start_command)],
         states={
-            0 : [(CommandHandler("create", creating_form))],
+            1 : [MessageHandler(Filters.text, answering)]
+        },
+        fallbacks= [MessageHandler(Filters.regex("Cancel"),cancel_command)]
+    ))
+
+    d.add_handler(CommandHandler("view_forms", view_forms))
+    d.add_handler(ConversationHandler(
+        entry_points=[(CommandHandler("create", creating_form))],
+        states={
             1 : [MessageHandler(Filters.text, title_of_form)],
             2: [MessageHandler(Filters.regex('[0-9]'), no_of_questions)],
             3 : [MessageHandler(Filters.text, questions_started)],
             4 : [MessageHandler(Filters.text, answering)]
         },
-        fallbacks= [MessageHandler(Filters.regex("Cancel"),cancel_command)]   
+        fallbacks= [MessageHandler(Filters.regex("Cancel"),cancel_command)]
     ))
     
     updater.start_polling()
