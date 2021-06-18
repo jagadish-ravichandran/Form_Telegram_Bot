63916cc7acef0eb4b54661d0c11cec8625cf987d
Jagadish R <goldjagadish@gmail.com>
2021-06-18 21:52:09 +0530

    answering completed

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 from telegram.ext import ConversationHandler, CallbackContext
 import sqlite3
-from telegram import Update
+from telegram import Update, user
 import logging
 
 def show_table(db : sqlite3.Connection, name : str):
@@ -10,40 +10,45 @@ def db_connect():
     db_con = sqlite3.connect('form_bot_db')
     return db_con
 
+def beginning(update: Update, context : CallbackContext):
+    update.effective_message.reply_text("I m here for creating forms")
+    update.effective_message.reply_text("Type /create to start creating")
+    return ConversationHandler.END
+
 def start_command(update : Update, context : CallbackContext):
     db = db_connect()
     cur = db.cursor()
     userid = int(update.effective_user.id)
     
-
     if not context.args:
         cur = db.execute(f'select * from user_table where user_id={userid}')
         if len(cur.fetchall()) == 0:
             cur = db.execute(f'insert into user_table values({userid},0)')
             db.commit()
             db.close()
-        update.effective_message.reply_text("I m here for creating forms")
-        update.effective_message.reply_text("Type /create to start creating")
-        return ConversationHandler.END
+            return beginning(update, context)
 
     else:
         ownerid, formid = list(map(int, context.args[0].split("_")))
-        print(ownerid)
-        print(formid)
         current_form = extract_form(formid,ownerid)
 
         if current_form is None:
-            update.effective_message.reply_text("The form is not available! \n Please check the form link !")
-            update.effective_message.reply_text("I m here for creating forms")
-            return ConversationHandler.END
-        print(current_form)
+            db.close()
+            return beginning(update, context)
+
+        cur = db.execute(f"select distinct form_id from answer_table where user_id={userid} and form_id = {formid}")
+        if cur.fetchone():
+            update.effective_message.reply_text("You answered this form already !")
+            db.close()
+            return beginning(update, context)
+
         context.user_data['form'] = current_form
         context.user_data['answers'] = []
         context.user_data['qns_to_answer'] = current_form[0][0]
         update.effective_message.reply_text(f"Form title : {current_form[0][2]}")
         update.effective_message.reply_text("Answer one by one for the following questions : ")
-        update.effective_message.reply_text(f"1. {current_form[0][4]}")
         context.user_data["answer_count"] = 0
+        update.effective_message.reply_text(f"1. {current_form[0][4]}")
         return 1
 
 def creating_form(update : Update, context : CallbackContext):
@@ -59,16 +64,16 @@ def title_of_form(update : Update, context : CallbackContext):
     return 2
 
 
-
 def storing_answers(update : Update, context : CallbackContext):
     db = db_connect()
     cur = db.cursor()
     user_id = update.effective_user.id
-    form_id = context.user_data["form"]
+    name = update.effective_user.full_name
+    form_id = context.user_data["form"][0][1]
     answers = context.user_data["answers"]
     count = context.user_data["answer_count"]
     for i in range(count):
-        cur = db.execute("insert into answer_table values(?,?,?)",user_id,form_id,answers[i])
+        db.execute("insert into answer_table values(?,?,?,?)",(user_id,name,form_id,answers[i]))
     db.commit()
     db.close()
 
@@ -77,39 +82,42 @@ def answering(update : Update, context : CallbackContext):
     current_form = context.user_data["form"]
     qcount = context.user_data["qns_to_answer"]
     answers = context.user_data["answers"]
+    ans_count = context.user_data["answer_count"]
 
-    if qcount == context.user_data["answer_count"]:
+    if qcount-1 == ans_count:
+        answers.append(update.effective_message.text)
+        context.user_data["answer_count"] += 1
+        ans_text = "Your answers are : \n"
+        for i in answers:
+            ans_text = ans_text + f"{(answers.index(i)+1)}. {i}\n"
+        update.effective_message.reply_text(ans_text)
         storing_answers(update, context)
-        update.effective_message.reply_text("You have answered all the questions!\nYour answeres are saved ! \nThank You! ")
-        update.effective_message.reply_text("I m here for creating forms")
-        update.effective_message.reply_text("Type /create to start creating")
-        return ConversationHandler.END
+        update.effective_message.reply_text("Your answeres are saved ! \nThank You! ")
+        return beginning(update, context)
 
     else:
         answers.append(update.effective_message.text)
         context.user_data["answer_count"] += 1
-        next_question = current_form[3][4]
-        update.effective_message.reply_text(f". {next_question}")
+        ans_count = context.user_data["answer_count"]
+        next_question = current_form[ans_count][4]
+        update.effective_message.reply_text(f"{ans_count+1}. {next_question}")
         return 1
 
 
 def no_of_questions(update : Update, context : CallbackContext):
-   
     question_count = int(update.message.text)
-
     context.user_data['question_count'] = question_count
     context.user_data['current_question'] = 1
     context.user_data['questions'] = []
     update.effective_message.reply_text("Enter questions line by line : ")
     update.effective_message.reply_text(f"Enter your question number {context.user_data['current_question']}")
-    
     return 3
 
 
 def questions_started(update : Update, context : CallbackContext):
     question = update.effective_message.text
     context.user_data['questions'].append(question)
-    logging.info(context.user_data)
+    #logging.info(context.user_data)
 
 
     if context.user_data['current_question'] == context.user_data['question_count']:
@@ -211,7 +219,13 @@ def view_forms(update : Update, context : CallbackContext):
     userid = update.effective_user.id
     flist = extract_form(formid= None, userid= userid)
     displaying_each_form(update, context, flist)
-            
+
+
+def show_answers(update : Update, context : CallbackContext):
+    userid = update.effective_user.id
+    flist = extract_form(formid= None, userid= userid)
+    displaying_each_form(update, context, flist)
+
 
 def cancel_command(update : Update, context : CallbackContext):
    update.effective_message.reply_text("Your current operation is cancelled")
