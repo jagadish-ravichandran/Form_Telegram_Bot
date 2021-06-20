0a9e0e7b1a9368e08d86a129360642864d12387f
Jagadish R <goldjagadish@gmail.com>
2021-06-20 14:18:37 +0530

    combined two convo handlers to one convo

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from telegram.files.file import File
 
 from functions import (
     db_connect,
+    help_command,
     invalid_qn_number,
     invalid_title,
     invalid_typing_in_answers,
@@ -23,6 +24,9 @@ from functions import (
     questions_started,
     answering,
     title_of_form,
+    typing_commands_in_CH,
+    unknown_commands,
+    unknown_messages,
     view_forms,
 )
 
@@ -36,10 +40,11 @@ logging.basicConfig(
     filename="logs.log",
     filemode="w",
     format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
-    level=logging.INFO,
+    level=logging.DEBUG,
 )
 
 logger = logging.getLogger(__name__)
+
 
 def db_intialize(db: sqlite3.Connection):
     cur = db.cursor()
@@ -113,20 +118,33 @@ def main():
 
     d = updater.dispatcher
 
-    d.add_handler(
+    """ d.add_handler(
         ConversationHandler(
             entry_points=[CommandHandler("start", start_command)],
-            states={1: [MessageHandler(Filters.text, answering)]},
-            fallbacks=[MessageHandler(Filters.all, invalid_typing_in_answers)],
-        )
-    )
-
-    d.add_handler(CommandHandler("view_forms", view_forms))
-    d.add_handler(
-        ConversationHandler(
-            entry_points=[(CommandHandler("create", creating_form))],
             states={
                 1: [
+                    MessageHandler(Filters.command, typing_commands_in_CH),
+                    MessageHandler(Filters.text, answering),
+                ]
+            },
+            fallbacks=[MessageHandler(Filters.all, invalid_typing_in_answers)],
+        )
+    ) """
+
+    d.add_handler(
+        ConversationHandler(
+            entry_points=[
+                (CommandHandler("create", creating_form)),
+                CommandHandler("start", start_command),
+            ],
+            states={
+                0: [
+                    MessageHandler(Filters.command, typing_commands_in_CH),
+                    MessageHandler(Filters.text, answering),
+                    MessageHandler(Filters.all, invalid_typing_in_answers),
+                ],
+                1: [
+                    MessageHandler(Filters.command, typing_commands_in_CH),
                     MessageHandler(
                         Filters.text & ~Filters.regex("Cancel"), title_of_form
                     ),
@@ -135,12 +153,14 @@ def main():
                     ),
                 ],
                 2: [
+                    MessageHandler(Filters.command, typing_commands_in_CH),
                     MessageHandler(Filters.regex("[0-9]"), no_of_questions),
                     MessageHandler(
                         Filters.all & ~Filters.regex("Cancel"), invalid_qn_number
                     ),
                 ],
                 3: [
+                    MessageHandler(Filters.command, typing_commands_in_CH),
                     MessageHandler(
                         Filters.text & ~Filters.regex("Cancel"), questions_started
                     ),
@@ -154,13 +174,18 @@ def main():
         )
     )
 
+    d.add_handler(CommandHandler("view_forms", view_forms))
     d.add_handler(CommandHandler("answers", show_answers))
+    d.add_handler(CommandHandler("help", help_command))
+    d.add_handler(MessageHandler(Filters.command, unknown_commands))
+    d.add_handler(MessageHandler(Filters.all, unknown_messages))
     updater.bot.set_my_commands(
         [
             BotCommand("start", "Start Me"),
             BotCommand("create", "Form Creation"),
             BotCommand("view_forms", "Your Forms"),
             BotCommand("answers", "Answers for your Forms"),
+            BotCommand("help", "Available commands")
         ]
     )
     updater.start_polling()
