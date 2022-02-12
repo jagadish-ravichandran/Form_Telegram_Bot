91df6cea8687450c9ab0e1ca8f12858b2b4e7b85
Karthik <Karthikraja2k1@gmail.com>
2022-02-12 22:42:10 +0530

    removed csv_files sub directory

--- Functions/database.py
+++ Functions/database.py
@@ -270,7 +270,7 @@ def creating_csv_for_each_form(form_records, userid):
         ans_dict[j[0]].append(j[1])
         tracker += 1
 
-    filename = f"csv_files/answers_{userid}:{formid}.csv"
+    filename = f"answers_{userid}:{formid}.csv"
 
     qn = Questions.get_questions(formid, userid)
 
